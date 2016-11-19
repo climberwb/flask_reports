054b49fc8dbaa05aea43541b5dd273da55cb0ff3
Warren Kushner <warren.kushner@gmail.com>
2016-11-19 19:14:40 +0000

    added routes for aggregates

--- flaskr.py
+++ flaskr.py
@@ -17,6 +17,7 @@ CORS(app)
 client = pymongo.MongoClient('dbh44.mlab.com', 27447)
 db = client['attempts']
 db.authenticate('ImASmarty', '12345')
+db.games.ensure_index('gameId', unique=True)
 
 app.config['MONGO_DBNAME'] = 'attempts'
 app.config['DB_USERNAME'] = 'ImASmarty'
@@ -53,22 +54,127 @@ def get_one_star(gameId):
         output = "No such name"
     return jsonify({'result' : output})
 
-@app.route('/game', methods=['POST'])
-def add_star():
+@app.route('/games', methods=['POST'])
+def add_game():
     games = db.games
     gameId = request.json['gameId']
-    general_game_attributes = request.json['generalGameAttributes']
-    specific_game_attributes = request.json['specificGameAttributes']
+    general_game_attributes = ["userId","level"]
+    specific_game_attributes = request.json['specificGameAttributes']#request.json['specificGameAttributes']
     game_id = games.insert({'gameId': gameId, \
                             'generalGameAttributes': general_game_attributes, \
-                            'specificGameAttributes':specific_game_attributes
-        
+                            'specificGameAttributes':specific_game_attributes, \
+                            'attempts':[]
                             })
     new_game = games.find_one({'_id': game_id })
     new_game.pop('_id')
     new_game.pop('attempts')
     return jsonify({'result' : new_game})
+    
+@app.route('/games/<game_id>/', methods=['POST'])
+def add_attempt(game_id):
+    games = db.games
+    attempt = request.json["attempt"]
+    if len(attempt.keys()) >0:
+        games.update({"gameId":game_id}, \
+                    { "$push": {"attempts": attempt }
+        })
+        
+        new_game = games.find_one({'gameId': game_id })
+        new_game.pop('_id')
+    else:
+        new_game = "you must have params when submitting attempt"
+    return jsonify({'result' : new_game})  
+ 
+ 
+###### aggregate code ##################
+def specific_keys_for_attempts():
+    games = db.games
+    all_games = games.find({})
+    get_skeys = db.games.aggregate([
+           { "$unwind": "$specificGameAttributes" },
+             { "$group": {
+                 "_id":"$specificGameAttributes",
+                 },
+              }
+          ])
+    return get_skeys
+   
+@app.route('/games/aggregate/', methods=['GET'])
+def statistics_for_all_games():
+    get_skeys = specific_keys_for_attempts()
+          
+    group_options  = {
+                        "$group": {
+                            "_id":"$gameId" 
+                        }
+                    }
+                    
+    for key_obj in get_skeys:
+        key_string = key_obj['_id']
+        value_string = "$attempts.%s" %(key_string)
+        value_obj = { "$avg":value_string }
+        group_options["$group"][key_string] = value_obj
+    
+    game_instance_stats = db.games.aggregate([
+         { "$unwind": "$attempts" },
+         group_options,
+         {"$sort":{ "_id": 1 } },
+     ])
+
+    output = list(game_instance_stats)
+    return jsonify({'result' : output})  
+
+@app.route('/games/aggregate_per_level/', methods=['GET'])
+def statistics_for_all_games_per_level():
+    get_skeys = specific_keys_for_attempts()
+    group_options  = {
+                        "$group": {
+                            "_id":{"gameId":"$gameId","level":"$attempts.level"}
+                        }
+                    }
+    
+    aggregate_by_level =   { "$group": { 
+                      "_id" :  "$_id.gameId",
+                      "levels": { 
+                          "$push": { 
+                             "level":"$_id.level"
+                             }
+                        }
+                    }    
+                }
+                  
+    for key_obj in get_skeys:
+        #### build group_options dict
+        key_string = key_obj['_id']
+        value_string = "$attempts.%s" %(key_string)
+        value_obj = { "$avg":value_string }
+        group_options["$group"][key_string] = value_obj
+        
+        ###build aggregate_by_level dict ####
+        aggregate_by_level["$group"]["levels"]["$push"][key_string] = "$%s"%(key_string)
+    
+    game_instance_stats = db.games.aggregate([
+         { "$unwind": "$attempts" },
+         group_options,
+         aggregate_by_level,
+         {"$sort":{ "_id": 1 } },
+     ])
+
+    output = list(game_instance_stats)
+    return jsonify({'result' : output})
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+
+'''
+create a new game
+
+curl -i -H "Content-Type: application/json" -X POST \
+ 'localhost:8080/games' -d '{"gameId":"game3", "generalGameAttributes":["userId","level"],\
+ "specificGameAttributes":["timeDeltaInSeconds","points","errors"]}'
+ 
+add attempts to existing game
+curl -i -H "Content-Type: application/json" -X POST  \
+ 'localhost:8080/games/game4/' -d '{"attempt":{"timeDeltaInSeconds":34,"points":3,"errors":1,"userId":3,"level":2}}'
+'''
